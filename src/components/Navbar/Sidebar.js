4d067a48b9b646cc6fda2191686450f730603d28
Aliakwe <aliakwe@yahoo.com>
2020-01-18 23:58:10 +0100

    implemented sidebar and mobile responsiveness

--- src/components/Navbar/Sidebar.js
+++ src/components/Navbar/Sidebar.js
@@ -1,12 +1,51 @@
 /** @format */
 
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "../../css/Sidebar.css";
 
 class Sidebar extends Component {
+  //   closeButton = () => {
+  //     document.getElementById("mySideNav").style.width = "0px";
+  //     // document.querySelector(".open-bars").style.display = "none";
+  //   };
+
+  //   openButton = () => {
+  //     document.getElementById("mySideNav").style.width = "300px";
+  //   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContent: true
+    };
+  }
+  onToggleClick = () => {
+    this.setState({ showContent: !this.state.showContent });
+  };
+
   render() {
     return (
-      <div>
-        <h2>hello side bar</h2>
+      <div className="sidebar">
+        <div className="sidebar-des">
+          <div>
+            <h1>hello</h1>
+            <h3>jjjjj</h3>
+          </div>
+        </div>
+
+        <div className="sidebar-mob">
+          <div className="sidebar-mob-item">
+            {this.state.showContent ? (
+              <ul className="sidebar-item-content">
+                <li>hello</li>
+                <li>whats</li>
+                <li>jhhhh</li>
+              </ul>
+            ) : null}
+          </div>
+          <div onClick={this.onToggleClick}>click</div>
+        </div>
       </div>
     );
   }
--- src/components/Transmonitors.js
+++ src/components/Transmonitors.js
@@ -1,12 +1,13 @@
 /** @format */
 
 import React, { Component } from "react";
+import "../css/Trans.css";
 
 class Transmonitors extends Component {
   render() {
     return (
-      <div>
-        <h1>transfer</h1>
+      <div className="container-trans">
+        <h1>transfer111</h1>
       </div>
     );
   }
