a67f6b3a98ae5f0e7a93850f7664bcd0fdaced7c
Aliakwe <aliakwe@yahoo.com>
2020-01-18 13:24:57 +0100

    just initialized the application

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+import "./App.css";
+import Transmonitors from "./components/Transmonitors";
+
+function App() {
+  return (
+    <div className="App">
+      <Transmonitors />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Transmonitors.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class Transmonitors extends Component {
+  render() {
+    return (
+      <div>
+        <h1>transfer</h1>
+      </div>
+    );
+  }
+}
+
+export default Transmonitors;
